3c1b4b31fba03cb52f4bef284b5434670113c155
eunji0j <2321031@slp.education>
2026-04-30 09:51:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import sys
 !{sys.executable} -m pip install konlpy
-
 from konlpy.tag import Okt
 
 def calculate_ttr(text):
@@ -23,7 +22,6 @@ def calculate_ttr(text):
     return ttr, list(unique_types), content_morphemes
 
 print("TTR 계산 함수가 정의되었습니다.")
-
 user_utterance = input("분석할 한국어 발화를 입력하세요: ")
 
 ttr_value, types, tokens = calculate_ttr(user_utterance)
